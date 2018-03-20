9b5cc986130bed2968ba9e3aa66c46b98152aae8
Paul Jeanbourquin <Wolfwalker96>
2018-03-20 22:12:03 +0100

    Improve isolate script

--- 02-Application/isolate.sh
+++ 02-Application/isolate.sh
@@ -1,3 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo chroot /home/paul/chroot $@
+CHROOTDIR=isolate
+
+ROOTDIRS=(bin lib lib32 lib64)
+
+for dir in "${ROOTDIRS[@]}"; do
+	mkdir -p $HOME/$CHROOTDIR/$dir
+	sudo mount -o bind /$dir $HOME/$CHROOTDIR/$dir
+done;
+
+sudo mkdir -p $HOME/$CHROOTDIR$HOME
+
+sudo chroot $HOME/$CHROOTDIR $@
